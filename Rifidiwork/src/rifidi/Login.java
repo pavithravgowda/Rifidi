b1e5c74298c1cbc3055667dbf565dcbf2625584d
pavithravgowda <36875154+pavithravgowda@users.noreply.github.com>
2018-02-27 15:00:42 +0530

    Update Login.java

--- Rifidiwork/src/rifidi/Login.java
+++ Rifidiwork/src/rifidi/Login.java
@@ -5,7 +5,7 @@ public class Login {
 	public static void main(String[] args) {
 		System.out.println("Username: Name");
 		System.out.println("Password: 123");
-
+		System.out.println("email: name@gmail.com");
 	}
 
 }
